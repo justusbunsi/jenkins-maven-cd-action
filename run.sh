cc144a67ddc6ea116e038a99f1cb76c2cf07554f
justusbunsi <61625851+justusbunsi@users.noreply.github.com>
2021-11-21 16:47:44 +0100

    Re-enable gh commands

--- run.sh
+++ run.sh
@@ -14,7 +14,6 @@ fi
 export MAVEN_OPTS=-Djansi.force=true
 mvn -B -V -s $GITHUB_ACTION_PATH/settings.xml -ntp -Dstyle.color=always $MVN_CHANGELIST_OPTION -Pquick-build -P\!consume-incrementals clean verify
 version=$(mvn -B -ntp $MVN_CHANGELIST_OPTION -Dexpression=project.version -q -DforceStdout help:evaluate)
-echo "${version}"
-echo gh api -F ref=refs/tags/$version -F sha=$GITHUB_SHA /repos/$GITHUB_REPOSITORY/git/refs
+gh api -F ref=refs/tags/$version -F sha=$GITHUB_SHA /repos/$GITHUB_REPOSITORY/git/refs
 release=$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .id')
-echo gh api -X PATCH -F draft=false -F name=$version -F tag_name=$version /repos/$GITHUB_REPOSITORY/releases/$release
+gh api -X PATCH -F draft=false -F name=$version -F tag_name=$version /repos/$GITHUB_REPOSITORY/releases/$release
